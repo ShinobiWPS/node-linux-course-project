ba846b0b20a52fc4f1f7610fd57dbbf56c894b92
Alexandru Gherasim <alexandru.gherasim@grandparade.co.uk>
2022-02-04 17:43:55 +0100

    save

--- express-web-server/app.js
+++ express-web-server/app.js
@@ -1,11 +1,17 @@
 'use strict';
 const express = require('express');
 const createError = require('http-errors');
+const indexRoutes = require('./routes');
+const helloRoutes = require('./routes/hello');
+
 const app = express();
+
+app.use('/', indexRoutes);
+app.use('/hello', helloRoutes);
 
 app.use((req, res, next) => {
   if (req.method !== 'GET') {
-    next(createError(405, 'Method not allowed'));
+    next(createError(405));
     return;
   }
   next(createError(404));
