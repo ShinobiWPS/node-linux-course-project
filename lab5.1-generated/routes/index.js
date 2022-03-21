2b5ff7d04ce6c7f9b0d5bda5452c49321e970c1c
Alex Ghera <axanderghemov@gmail.com>
2022-03-21 20:51:08 +0100

    save

---
+++ lab5.1-generated/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+const { boat } = require('../model');
+var router = express.Router({ mergeParams: true });
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  console.log(req.params);
+  console.log(req.query);
+  boat.read(req.params.id, (err, result) => {
+    if (err) {
+      if (err.message === 'not found') next();
+      else next(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+module.exports = router;
