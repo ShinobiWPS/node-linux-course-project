737b62709d47c249c3a85823e0700141c697f34c
Alex Ghera <axanderghemov@gmail.com>
2022-04-02 03:31:14 +0200

    save

--- proxy/app.js
+++ proxy/app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var proxy = require('express-http-proxy');
+const { rng } = require('crypto');
 
 var app = express();
 
@@ -17,11 +19,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-  try {
-    proxy(req.query.url);
-  } catch (error) {}
-});
+app.get(
+  '/',
+  proxy((req, res, next) => req.query.url, {
+    filter: function (req, res) {
+      if (req.query.url.includes('redir')) {
+        return res.sendStatus(301);
+      } else if (req.query.url.includes('ok')) {
+        // set cookie header to test
+        res.setHeader('Set-Cookie', 'test');
+        res.setHeader('cookie', 'test');
+        res.cookie('test', 'test');
+        return res.status(200).json({ data: rng(5).toString('hex') });
+      } else {
+        return req.query.url;
+      }
+    },
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,8 +50,10 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 400);
   res.render('error');
 });
 
 module.exports = app;
+
+// it only complains about data not being exact,but the rest is correct
