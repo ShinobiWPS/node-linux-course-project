116b45b973ea5a2d703d14cdf0626e1b32b6e1df
Alex Ghera <axanderghemov@gmail.com>
2022-04-06 20:21:44 +0200

    save

---
+++ proxy-full/app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const API_SERVICE_URL = 'https://jsonplaceholder.typicode.com';
+app.use(
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
