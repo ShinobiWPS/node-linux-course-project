550c444517f6e72f084fc125b8288eafba5e77a8
ShinobiWPS <axanderghemov@gmail.com>
2022-06-08 09:55:58 +0200

    save

--- lab10.1/routes/index.js
+++ lab10.1/routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.ip)
+  if (req.socket.remoteAddress === '111.34.55.211') {
+    res.status(403).send()
+    return
+  }
   res.render('index', { title: 'Express' });
 });
 
